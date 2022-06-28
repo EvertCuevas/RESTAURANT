78b448c5f49c6019cba5ed3d851385ca4a10e5c4
Evert Cuevas <evert.cuevas@ucb.edu.bo>
2022-06-27 22:10:47 -0400

    segunda parte

--- app/Comida.php
+++ app/Comida.php
@@ -3,4 +3,23 @@ namespace App;
 
 class Comida
 {
+    private $nombre;
+    public $precio;
+
+    public function ObtenerNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function EstablecerNombre($nuevovalor)
+    {
+        $nombre = $nuevovalor;
+        return $nombre;
+    }
+
+    public function __construct()
+    {
+        $this->nombre = "s/n";
+        $this->precio = 5;    
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,2 +1,16 @@
 <?php
 echo "Hola Mundo";
+
+use App\Comida;
+use App\Maria\Pepe;
+
+$objetoUno=new Comida();
+$objetoUno->precio = 5;
+$objetoUno->EstablecerNombre("sopa");
+
+$objetoDos=new Comida();
+$objetoDos->precio = 10;
+$objetoDos->EstablecerNombre("majaop");
+
+
+$objetoTres=new Pepe();
